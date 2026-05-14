900fa885a26a3815bbf2fb6b6773804168c3bee4
ram-globalmagicko <ram@globalmagicko.com>
2026-05-14 14:17:37 +0700

    Updated the prisma adapter config' clear

--- src/lib/prisma.server.ts
+++ src/lib/prisma.server.ts
@@ -15,17 +15,13 @@ const adapter = new PrismaMariaDb({
   password: process.env.DATABASE_PASSWORD,
   port: Number(process.env.DATABASE_PORT),
   database: process.env.DATABASE_NAME,
-  connectionLimit: 5,
+  connectionLimit: 1,
+  connectTimeout: 30000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-console.log("PRISMA ADAPTER ARE", {
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  port: Number(process.env.DATABASE_PORT),
-  database: process.env.DATABASE_NAME,
-  connectionLimit: 5,
-});
 const prisma = new PrismaClient({ adapter });
 
 export { prisma, Meta, Education, Experience, Project, Testimonial };
