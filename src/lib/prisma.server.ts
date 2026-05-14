e44f61bc84d000ff4d5ad6f0151ceabc6e2a7a3a
ram-globalmagicko <ram@globalmagicko.com>
2026-05-14 14:12:25 +0700

    Updated the prisma log

--- src/lib/prisma.server.ts
+++ src/lib/prisma.server.ts
@@ -22,6 +22,7 @@ console.log("PRISMA ADAPTER ARE", {
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
+  port: Number(process.env.DATABASE_PORT),
   database: process.env.DATABASE_NAME,
   connectionLimit: 5,
 });
